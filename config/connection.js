66c94163ee97af1eb5f3ec2b83885977a7fb6fa4
Mike Galarneau <mbgalarneau@gmail.com>
2018-12-22 10:28:05 -0500

    added heroku mysql connection string; moved local to .env to hide from github

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Set up MySQL connection.
+var dotenv = require("dotenv").config();
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "jct286",
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+ connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
   database: "burgers_db"
 });
-
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
